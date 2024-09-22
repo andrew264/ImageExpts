bug fixes and add gradient accumulation for vqgan

model/vqgan/discriminator.py
@@ -15,6 +15,11 @@ def hinge_d_loss(logits_real, logits_fake):
 def vanilla_d_loss(logits_real, logits_fake):
     return 0.5 * (torch.mean(F.softplus(-logits_real)) + torch.mean(F.softplus(logits_fake)))
 
+def hinge_g_loss(logits_fake): return -torch.mean(logits_fake)
+
+
+def vanilla_g_loss(logits_fake): return torch.mean(F.softplus(-logits_fake))
+
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -31,22 +36,13 @@ class NLayerDiscriminator(nn.Module):
     """
 
     def __init__(self, input_nc=3, ndf=64, n_layers=3, ):
-        """Construct a PatchGAN discriminator
-        Parameters:
-            input_nc (int)  -- the number of channels in input images
-            ndf (int)       -- the number of filters in the last conv layer
-            n_layers (int)  -- the number of conv layers in the discriminator
-        """
         super(NLayerDiscriminator, self).__init__()
         norm_layer = nn.BatchNorm2d
         use_bias = norm_layer != nn.BatchNorm2d
 
         kw = 4
         padw = 1
-        sequence = [
-            nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw),
-            nn.LeakyReLU(0.2, True)
-        ]
+        sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
         nf_mult = 1
         nf_mult_prev = 1
         for n in range(1, n_layers):  # gradually increase the number of filters
@@ -66,8 +62,7 @@ class NLayerDiscriminator(nn.Module):
             nn.LeakyReLU(0.2, True)
         ]
 
-        sequence += [
-            nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
+        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
         self.main = nn.Sequential(*sequence)
 
     def forward(self, x):
@@ -83,18 +78,17 @@ class VQLPIPSWithDiscriminator(nn.Module):
         assert disc_loss in ["hinge", "vanilla"]
         self.codebook_weight = codebook_weight
         self.pixel_weight = pixelloss_weight
-        self.perceptual_loss = LearnedPerceptualImagePatchSimilarity(normalize=True).eval()
+        self.perceptual_loss = LearnedPerceptualImagePatchSimilarity(net_type='vgg', reduction='mean', normalize=False).eval()
         self.perceptual_weight = perceptual_weight
 
-        self.discriminator = NLayerDiscriminator(input_nc=disc_in_channels,
-                                                 n_layers=disc_num_layers,
-                                                 ndf=disc_ndf
-                                                 ).apply(weights_init)
+        self.discriminator = NLayerDiscriminator(input_nc=disc_in_channels, n_layers=disc_num_layers, ndf=disc_ndf).apply(weights_init)
         self.discriminator_iter_start = disc_start
         if disc_loss == "hinge":
             self.disc_loss = hinge_d_loss
+            self.gen_loss = hinge_g_loss
         elif disc_loss == "vanilla":
             self.disc_loss = vanilla_d_loss
+            self.gen_loss = vanilla_g_loss
         else:
             raise ValueError(f"Unknown GAN loss '{disc_loss}'.")
         print(f"VQLPIPSWithDiscriminator running with {disc_loss} loss.")
@@ -115,20 +109,16 @@ class VQLPIPSWithDiscriminator(nn.Module):
         d_weight = d_weight * self.discriminator_weight
         return d_weight
 
-    def forward(self, codebook_loss, inputs, reconstructions, optimizer_idx,
-                global_step, last_layer=None, cond=None, split="train"):
+    def forward(self, codebook_loss, inputs, reconstructions, optimizer_idx, global_step, last_layer=None, cond=None, split="train"):
         rec_loss = torch.abs(inputs.contiguous() - reconstructions.contiguous())
         if self.perceptual_weight > 0:
-            # clamp tensors to 0-1
-            inputs = torch.clamp(inputs, 0.0, 1.0)
-            reconstructions = torch.clamp(reconstructions, 0.0, 1.0)
+            # clamp tensors to -1 to 1
+            reconstructions = torch.clamp(reconstructions, -1.0, 1.0)
             p_loss = self.perceptual_loss(inputs.contiguous(), reconstructions.contiguous())
-            rec_loss = rec_loss + self.perceptual_weight * p_loss
         else:
             p_loss = torch.tensor([0.0])
 
-        nll_loss = rec_loss
-        # nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
+        nll_loss = rec_loss + self.perceptual_weight * p_loss
         nll_loss = torch.mean(nll_loss)
 
         # now the GAN part
@@ -140,7 +130,7 @@ class VQLPIPSWithDiscriminator(nn.Module):
             else:
                 assert self.disc_conditional
                 logits_fake = self.discriminator(torch.cat((reconstructions.contiguous(), cond), dim=1))
-            g_loss = -torch.mean(logits_fake)
+            g_loss = self.gen_loss(logits_fake)
 
             try:
                 d_weight = self.calculate_adaptive_weight(nll_loss, g_loss, last_layer=last_layer)
@@ -158,8 +148,7 @@ class VQLPIPSWithDiscriminator(nn.Module):
                    "{}/p_loss".format(split): p_loss.detach().mean(),
                    "{}/d_weight".format(split): d_weight.detach(),
                    "{}/disc_factor".format(split): torch.tensor(disc_factor),
-                   "{}/g_loss".format(split): g_loss.detach().mean(),
-                   }
+                   "{}/g_loss".format(split): g_loss.detach().mean(),}
             return loss, log
 
         if optimizer_idx == 1:
@@ -176,6 +165,5 @@ class VQLPIPSWithDiscriminator(nn.Module):
 
             log = {"{}/disc_loss".format(split): d_loss.clone().detach().mean(),
                    "{}/logits_real".format(split): logits_real.detach().mean(),
-                   "{}/logits_fake".format(split): logits_fake.detach().mean()
-                   }
+                   "{}/logits_fake".format(split): logits_fake.detach().mean()}
             return d_loss, log

model/vqgan/image_tokenizer.py
@@ -13,6 +13,7 @@ class ImageTokenizer:
     # https://github.com/facebookresearch/chameleon/blob/main/chameleon/inference/image_tokenizer.py
     def __init__(self, model: VQModel = None, device: str | torch.device | None = None,):
         self._vq_model = model
+        self._dtype = None
 
         if self._vq_model is not None:
             if device is None:
@@ -32,7 +33,7 @@ class ImageTokenizer:
             transforms.Resize(target_image_size, transforms.InterpolationMode.LANCZOS),
             transforms.PILToTensor(),
             transforms.CenterCrop(target_image_size),
-            transforms.ToDtype(torch.float32, scale=True),
+            transforms.ToDtype(self._dtype if self._dtype else torch.float32, scale=True),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
         ])
 
@@ -64,7 +65,8 @@ class ImageTokenizer:
     def pil_from_img_toks(self, img_tensor: Tensor) -> List[Image.Image]:
         emb_dim = self._vq_model.quantize.embedding.weight.shape[-1]
         if img_tensor.ndim == 3: img_tensor = img_tensor.unsqueeze(0)
-        bz = img_tensor.shape[0]
-        codebook_entry = self._vq_model.quantize.get_codebook_entry(img_tensor, (bz, 32, 32, emb_dim))
+        bz, num_tokens = img_tensor.size()
+        out_size = int(num_tokens ** 0.5)
+        codebook_entry = self._vq_model.quantize.get_codebook_entry(img_tensor, (bz, out_size, out_size, emb_dim))
         pixels = self._vq_model.decode(codebook_entry)
         return self._pil_from_chw_tensor(pixels)

model/vqgan/vqgan.py
@@ -31,19 +31,20 @@ loss_config = MappingProxyType(dict(
 
 class VQModel(L.LightningModule):
     # https://github.com/CompVis/taming-transformers/blob/master/taming/models/vqgan.py
-    def __init__(self, ddconfig=dd_config,  lossconfig: Optional[dict] = None, n_embed: int=8192, embed_dim: int=256, colorize_nlabels=None, monitor=None, remap=None, sane_index_shape=False,):
+    def __init__(self, ddconfig=dd_config,  lossconfig: Optional[dict] = None, n_embed: int=8192, embed_dim: int=256, colorize_nlabels=None, monitor=None, remap=None, sane_index_shape=False, grad_accum_steps: int=1):
         super().__init__()
         self.encoder = Encoder(**ddconfig)
         self.decoder = Decoder(**ddconfig)
         self.quantize = VectorQuantizer(n_embed, embed_dim, beta=0.25, remap=remap, sane_index_shape=sane_index_shape)
         self.quant_conv = torch.nn.Conv2d(ddconfig["z_channels"], embed_dim, 1)
         self.post_quant_conv = torch.nn.Conv2d(embed_dim, ddconfig["z_channels"], 1)
-        self.learning_rate = 2e-4
+        self.learning_rate = 5e-4
         if colorize_nlabels is not None: self.register_buffer("colorize", torch.randn(3, colorize_nlabels, 1, 1))
         self.monitor = monitor
         self.loss = VQLPIPSWithDiscriminator(**lossconfig) if lossconfig is not None else None
 
         self.automatic_optimization = False
+        self.grad_accum_steps = grad_accum_steps
 
     def encode(self, x): return self.quantize(self.quant_conv(self.encoder(x)))
 
@@ -60,8 +61,8 @@ class VQModel(L.LightningModule):
         if x.ndim == 3: x = x.unsqueeze(0)
         return x.contiguous()
 
-    def training_step(self, x, **kwargs):
-        x = self.get_input(x)
+    def training_step(self, batch, batch_idx):
+        x = self.get_input(batch)
         xrec, qloss = self(x)
 
         opt_ae, opt_disc = self.optimizers()
@@ -70,9 +71,11 @@ class VQModel(L.LightningModule):
         # Optimize Generator #
         ######################
         aeloss, log_dict_ae = self.loss(qloss, x, xrec, 0, self.global_step, last_layer=self.get_last_layer(), split="train")
-        opt_ae.zero_grad()
-        self.manual_backward(aeloss)
-        opt_ae.step()
+        self.manual_backward(aeloss / self.grad_accum_steps)
+
+        if (batch_idx + 1) % self.grad_accum_steps == 0:
+            opt_ae.step()
+            opt_ae.zero_grad()
 
         self.log("train/aeloss", aeloss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
         self.log_dict(log_dict_ae, prog_bar=False, logger=True, on_step=True, on_epoch=True)
@@ -81,10 +84,11 @@ class VQModel(L.LightningModule):
         # Optimize Discriminator #
         ##########################
         discloss, log_dict_disc = self.loss(qloss, x, xrec, 1, self.global_step, last_layer=self.get_last_layer(), split="train")
+        self.manual_backward(discloss / self.grad_accum_steps)
 
-        opt_disc.zero_grad()
-        self.manual_backward(discloss)
-        opt_disc.step()
+        if (batch_idx + 1) % self.grad_accum_steps == 0:
+            opt_disc.step()
+            opt_disc.zero_grad()
 
         self.log("train/discloss", discloss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
         self.log_dict(log_dict_disc, prog_bar=False, logger=True, on_step=True, on_epoch=True)
@@ -104,28 +108,8 @@ class VQModel(L.LightningModule):
 
     def configure_optimizers(self):
         lr = self.learning_rate
-        opt_ae = torch.optim.Adam(list(self.encoder.parameters())+list(self.decoder.parameters())+list(self.quantize.parameters())+list(self.quant_conv.parameters())+list(self.post_quant_conv.parameters()), lr=lr, betas=(0.5, 0.9))
-        opt_disc = torch.optim.Adam(self.loss.discriminator.parameters(), lr=lr, betas=(0.5, 0.9))
+        opt_ae = torch.optim.AdamW(list(self.encoder.parameters())+list(self.decoder.parameters())+list(self.quantize.parameters())+list(self.quant_conv.parameters())+list(self.post_quant_conv.parameters()), lr=lr, betas=(0.5, 0.9))
+        opt_disc = torch.optim.AdamW(self.loss.discriminator.parameters(), lr=lr, betas=(0.5, 0.9))
         return [opt_ae, opt_disc], []
 
-    def get_last_layer(self):
-        return self.decoder.conv_out.weight
-
-    def log_images(self, batch, **kwargs):
-        log = dict()
-        x = self.get_input(batch).to(self.device)
-        xrec, _ = self(x)
-        if x.shape[1] > 3:
-            x = self.to_rgb(x)
-            xrec = self.to_rgb(xrec)
-        log["inputs"] = x
-        log["reconstructions"] = xrec
-        return log
-
-    def to_rgb(self, x):
-        assert self.image_key == "segmentation"
-        if not hasattr(self, "colorize"):
-            self.register_buffer("colorize", torch.randn(3, x.shape[1], 1, 1).to(x))
-        x = F.conv2d(x, weight=self.colorize)
-        x = 2. * (x - x.min()) / (x.max() - x.min()) - 1.
-        return x
+    def get_last_layer(self): return self.decoder.conv_out.weight

train_vqgan.py
@@ -3,16 +3,32 @@ import os
 import datasets
 import lightning as L
 import torch
+from PIL import Image
 from torch.utils.data import DataLoader
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 from model import VQModel
 from model.vqgan.image_tokenizer import ImageTokenizer
 
 torch.set_float32_matmul_precision('high')
 
-
+class Debug(L.Callback):
+    def __init__(self):
+        self.tokenizer: ImageTokenizer = None
+        self.image = Image.open('data/test.png')
+    def setup(self, trainer, pl_module, stage):
+        self.tokenizer = ImageTokenizer(pl_module)
+    def on_train_batch_end(self, trainer: L.Trainer, pl_module, outputs, batch, batch_idx):
+        step = batch_idx
+        if step > 0 and step % 500 == 0:
+            tokens = self.tokenizer.img_tokens_from_pil(self.image)
+            with open('data/tokens.txt', mode='a', encoding='utf-8') as f: f.write(f"step_{step}: " + str(tokens[0].tolist()) + '\n')
+            img = self.tokenizer.pil_from_img_toks(tokens)[0]
+            img.save(f'data/test_step_{step}.png')
+            
 def train(model: L.LightningModule, dataloader: DataLoader):
-    trainer = L.Trainer(max_epochs=1, precision="bf16-true", )
+    checkpoint_callback = ModelCheckpoint(dirpath="weights/vqgan", filename="weights", save_weights_only=True, every_n_train_steps=5000)
+    trainer = L.Trainer(max_epochs=1, precision="bf16-true", callbacks=[checkpoint_callback, Debug()])
     trainer.fit(model, dataloader)
     sd = model.state_dict()
     for key in list(sd.keys()):
@@ -21,9 +37,10 @@ def train(model: L.LightningModule, dataloader: DataLoader):
 
 
 if __name__ == '__main__':
-    dd_config = dict(double_z=False, z_channels=256, resolution=512, in_channels=3, out_ch=3, ch=128, ch_mult=[1, 1, 2, 2, 4, 4], num_res_blocks=2, attn_resolutions=[],)
-    l_cfg = dict(disc_conditional=False, disc_in_channels=3, disc_start=10000, disc_weight=0.8, codebook_weight=1.0,)
-    model = VQModel(dd_config, n_embed=2**15, lossconfig=l_cfg)
+    dd_config = dict(double_z=False, z_channels=256, resolution=512, in_channels=3, out_ch=3, ch=128, ch_mult=[1, 1, 2, 2, 4], num_res_blocks=2, attn_resolutions=[],)
+    l_cfg = dict(disc_conditional=False, disc_in_channels=3, disc_start=100000, disc_weight=0.8, codebook_weight=1.0,)
+    model = VQModel(dd_config, n_embed=2**14, lossconfig=l_cfg, grad_accum_steps=4).bfloat16().cuda()
+    # model.forward = torch.compile(model=model.forward,)
     print(model)
 
     path = "/home/andrew264/datasets/text-to-image-2M/"